bcc1b17cf9797ca915e8aa6663758a2c807b414b
RENANYANG <ryanren991128@gmail.com>
2024-07-19 18:59:23 -0400

    Update project with latest SwiftUI implementation and documentation integration

--- Assignment1/DetailViewModel.swift
+++ Assignment1/DetailViewModel.swift
@@ -18,16 +18,71 @@ class DetailViewModel: ObservableObject {
     // Returns the documentation URL based on the component type
     var documentationURL: URL {
         let baseURL = "https://developer.apple.com/documentation/swiftui/"
+        let componentURL: String
+        
         switch item.title {
         case "Button":
-            return URL(string: baseURL + "button")!
+            componentURL = "button"
+        case "Label":
+            componentURL = "label"
         case "TextField":
-            return URL(string: baseURL + "textfield")!
-        // Add more cases as needed
+            componentURL = "textfield"
+        case "SecureField":
+            componentURL = "securefield"
+        case "TextArea":
+            componentURL = "texteditor"  // Note: SwiftUI does not have a TextArea; TextEditor is used instead
+        case "Image":
+            componentURL = "image"
+        case "Menu":
+            componentURL = "menu"
+        case "Link":
+            componentURL = "link"
+        case "Slider":
+            componentURL = "slider"
+        case "Stepper":
+            componentURL = "stepper"
+        case "Toggle":
+            componentURL = "toggle"
+        case "Picker":
+            componentURL = "picker"
+        case "DatePicker":
+            componentURL = "datepicker"
+        case "ColorPicker":
+            componentURL = "colorpicker"
+        case "ProgressView":
+            componentURL = "progressview"
+        case "HStack":
+            componentURL = "hstack"
+        case "VStack":
+            componentURL = "vstack"
+        case "ZStack":
+            componentURL = "zstack"
+        case "Form":
+            componentURL = "form"
+        case "NavigationView":
+            componentURL = "navigationview"
+        case "Alerts":
+            componentURL = "alert"
+        case "Sheets":
+            componentURL = "sheet"
+        case "Plain List":
+            componentURL = "list"
+        case "Inset List":
+            componentURL = "list"
+        case "Grouped List":
+            componentURL = "list"
+        case "Inset Grouped List":
+            componentURL = "list"
+        case "Sidebar":
+            componentURL = "sidebar"
         default:
-            return URL(string: baseURL)!
+            componentURL = ""
         }
+        
+        let fullURL = baseURL + componentURL
+        return URL(string: fullURL)!
     }
 }
+
 
 
